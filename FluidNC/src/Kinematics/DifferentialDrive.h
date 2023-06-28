590a9cb84acaff3b50f1022237f407640625a578
Ben Elgin <elgin@cs.unc.edu>
2023-06-28 16:32:34 -0400

    Fixes to chatGPT's header so it compiles

--- FluidNC/src/Kinematics/DifferentialDrive.h
+++ FluidNC/src/Kinematics/DifferentialDrive.h
@@ -15,15 +15,17 @@ namespace Kinematics {
         virtual bool cartesian_to_motors(float* target, plan_line_data_t* pl_data, float* position) override;
         virtual void init() override;
         virtual void init_position() override;
-        void         motors_to_cartesian(float* cartesian, float* motors, int n_axis) override;
+        virtual void motors_to_cartesian(float* cartesian, float* motors, int n_axis) override;
+
+        virtual void transform_cartesian_to_motors(float* motors, float* cartesian) override;
 
         bool canHome(AxisMask axisMask) override;
         void releaseMotors(AxisMask axisMask, MotorMask motors) override;
         bool limitReached(AxisMask& axisMask, MotorMask& motors, MotorMask limited) override;
 
-        void afterParse() override {}
-        void group(Configuration::HandlerBase& handler) override;
-        void validate() override {}
+//        void afterParse() override {}
+//        void group(Configuration::HandlerBase& handler) override;
+//        void validate() override {}
 
         const char* name() const override { return "DifferentialDrive"; }
 
