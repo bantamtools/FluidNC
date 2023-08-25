167510cb9619ef08deb37fa070fea77e6c41246c
Matt Staniszewski <matt@msxconsulting.com>
2023-08-25 11:07:40 -0700

    Created List class

---
+++ FluidNC/src/List.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include "Config.h"
+
+#define LIST_NAME_MAX_STR   40
+#define LIST_NAME_MAX_PATH  255
+
+typedef struct ListNodeType
+{
+    // List neighbor attributes
+    struct ListNodeType *prev;
+    struct ListNodeType *next;
+
+    // Submenu attributes
+    struct ListType *child;
+
+    // List entry characteristics
+    char display_name[LIST_NAME_MAX_STR];
+    char path[LIST_NAME_MAX_PATH];
+    bool selected;
+    bool updated; // optional updated flag (used for RSS updates, etc)
+
+} ListNodeType;
+
+typedef struct ListType {
+    struct ListType *parent;
+    struct ListNodeType *head;
+    struct ListNodeType *active_head;
+} ListType;
+
+class List {
+
+protected:
+
+    void init(ListType *list, ListType *parent);
+    void add(ListType *list, ListType *sublist, const char *path, const char *display_name, bool updated = false);
+    void remove(ListType *list);
+
+public:
+
+    List();
+    ~List();
+};
