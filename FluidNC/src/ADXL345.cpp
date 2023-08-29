c0cfcb2c24a6be9083344da4851f4d08daa6f038
Matt Staniszewski <matt@msxconsulting.com>
2023-08-29 16:45:09 -0700

    Refactor 2

--- FluidNC/src/ADXL345.cpp
+++ FluidNC/src/ADXL345.cpp
@@ -9,24 +9,19 @@ ADXL345::ADXL345(uint8_t address, I2CBus* i2c) {
   _xyz[ACCEL_Z_AXIS] = 0;
 }
 
-const float ADXL345::kRatio2g  = (float) (2 * 2) / 1024.0f;
-const float ADXL345::kRatio4g  = (float) (4 * 2) / 1024.0f;
-const float ADXL345::kRatio8g  = (float) (8 * 2) / 1024.0f;
-const float ADXL345::kRatio16g = (float) (16 * 2) / 1024.0f;
-
 bool ADXL345::start() {
-  _powerCtlBits.measure = 1;
-  return write_register(REG_POWER_CTL, _powerCtlBits.toByte());
+  _power_ctl_bits.measure = 1;
+  return write_register(ADXL345_REG_POWER_CTL, _power_ctl_bits.to_byte());
 }
 
 bool ADXL345::stop() {
-  _powerCtlBits.measure = 0;
-  return write_register(REG_POWER_CTL, _powerCtlBits.toByte());
+  _power_ctl_bits.measure = 0;
+  return write_register(ADXL345_REG_POWER_CTL, _power_ctl_bits.to_byte());
 }
 
 uint8_t ADXL345::read_device_id() {
   uint8_t value = 0;
-  if (read_register(REG_DEVID, &value)) {
+  if (read_register(ADXL345_REG_DEVID, &value)) {
     return value;
   } else {
     return 0;
@@ -37,7 +32,7 @@ bool ADXL345::update() {
   // Read all axis registers (DATAX0, DATAX1, DATAY0, DATAY1, DATAZ0, DATAZ1) in accordance with data sheet.
   // "It is recommended that a multiple-uint8_t read of all registers be performed to prevent a change in data between reads of sequential registers."
   uint8_t values[6];
-  if (read_registers(REG_DATAX0, values, sizeof(values))) {
+  if (read_registers(ADXL345_REG_DATAX0, values, sizeof(values))) {
     // convert endian
     _xyz[ACCEL_X_AXIS] = (int16_t) word(values[1], values[0]);  // x
     _xyz[ACCEL_Y_AXIS] = (int16_t) word(values[3], values[2]);  // y
@@ -61,54 +56,54 @@ float ADXL345::get_gs(accel_axis_t axis) {
 }
 
 bool ADXL345::write_rate(uint8_t rate) {
-  _bwRateBits.lowPower = 0;
-  _bwRateBits.rate = rate & 0x0F;
-  return write_register(REG_BW_RATE, _bwRateBits.toByte());
+  _bw_rate_bits.low_power = 0;
+  _bw_rate_bits.rate = rate & 0x0F;
+  return write_register(ADXL345_REG_BW_RATE, _bw_rate_bits.to_byte());
 }
 
 bool ADXL345::write_rate_with_low_power(uint8_t rate) {
-  _bwRateBits.lowPower = 1;
-  _bwRateBits.rate = rate & 0x0F;
-  return write_register(REG_BW_RATE, _bwRateBits.toByte());
+  _bw_rate_bits.low_power = 1;
+  _bw_rate_bits.rate = rate & 0x0F;
+  return write_register(ADXL345_REG_BW_RATE, _bw_rate_bits.to_byte());
 }
 
 bool ADXL345::write_range(uint8_t range) {
-  _dataFormatBits.range = range & 0x03;
-  return write_register(REG_DATA_FORMAT, _dataFormatBits.toByte());
+  _data_format_bits.range = range & 0x03;
+  return write_register(ADXL345_REG_DATA_FORMAT, _data_format_bits.to_byte());
 }
 
-float ADXL345::convertToG(int16_t rawValue) {
-  switch (_dataFormatBits.range) {
+float ADXL345::convertToG(int16_t raw_value) {
+  switch (_data_format_bits.range) {
     case ADXL345_RANGE_2G:
-      return rawValue * kRatio2g;
+      return raw_value * k_ratio_2g;
 
     case ADXL345_RANGE_4G:
-      return rawValue * kRatio4g;
+      return raw_value * k_ratio_4g;
 
     case ADXL345_RANGE_8G:
-      return rawValue * kRatio8g;
+      return raw_value * k_ratio_8g;
 
     case ADXL345_RANGE_16G:
-      return rawValue * kRatio16g;
+      return raw_value * k_ratio_16g;
 
     default:
       return 0;
   }
 }
 
-float ADXL345::convertToMetersPerSec2(int16_t rawValue) {
-  switch (_dataFormatBits.range) {
+float ADXL345::convertToMetersPerSec2(int16_t raw_value) {
+  switch (_data_format_bits.range) {
     case ADXL345_RANGE_2G:
-      return rawValue * kRatio2g * ADXL345_GRAVITY_STD;
+      return raw_value * k_ratio_2g * ADXL345_GRAVITY_STD;
 
     case ADXL345_RANGE_4G:
-      return rawValue * kRatio4g * ADXL345_GRAVITY_STD;
+      return raw_value * k_ratio_4g * ADXL345_GRAVITY_STD;
 
     case ADXL345_RANGE_8G:
-      return rawValue * kRatio8g * ADXL345_GRAVITY_STD;
+      return raw_value * k_ratio_8g * ADXL345_GRAVITY_STD;
 
     case ADXL345_RANGE_16G:
-      return rawValue * kRatio16g * ADXL345_GRAVITY_STD;
+      return raw_value * k_ratio_16g * ADXL345_GRAVITY_STD;
 
     default:
       return 0;
@@ -154,51 +149,51 @@ bool ADXL345::write_register(uint8_t address, uint8_t value) {
 }
 
 ADXL345::PowerCtlBits::PowerCtlBits() {
-  this->link      = 0;
-  this->autoSleep = 0;
-  this->measure   = 0;
-  this->sleep     = 0;
-  this->wakeup    = 0;
+  this->link        = 0;
+  this->auto_sleep  = 0;
+  this->measure     = 0;
+  this->sleep       = 0;
+  this->wakeup      = 0;
 }
 
-uint8_t ADXL345::PowerCtlBits::toByte() {
+uint8_t ADXL345::PowerCtlBits::to_byte() {
   uint8_t bits = 0x00;
-  bits |= this->link      << 5;
-  bits |= this->autoSleep << 4;
-  bits |= this->measure   << 3;
-  bits |= this->sleep     << 2;
+  bits |= this->link        << 5;
+  bits |= this->auto_sleep  << 4;
+  bits |= this->measure     << 3;
+  bits |= this->sleep       << 2;
   bits |= this->wakeup;
   return bits;
 }
 
 ADXL345::DataFormatBits::DataFormatBits() {
-  this->selfTest  = 0;
-  this->spi       = 0;
-  this->intInvert = 0;
-  this->fullRes   = 0;
-  this->justify   = 0;
-  this->range     = 0;
+  this->self_test   = 0;
+  this->spi         = 0;
+  this->int_invert  = 0;
+  this->full_res    = 0;
+  this->justify     = 0;
+  this->range       = 0;
 }
 
-uint8_t ADXL345::DataFormatBits::toByte() {
+uint8_t ADXL345::DataFormatBits::to_byte() {
   uint8_t bits = 0x00;
-  bits |= this->selfTest  << 7;
-  bits |= this->spi       << 6;
-  bits |= this->intInvert << 5;
-  bits |= this->fullRes   << 3;
-  bits |= this->justify   << 2;
+  bits |= this->self_test   << 7;
+  bits |= this->spi         << 6;
+  bits |= this->int_invert  << 5;
+  bits |= this->full_res    << 3;
+  bits |= this->justify     << 2;
   bits |= this->range;
   return bits;
 }
 
 ADXL345::BwRateBits::BwRateBits() {
-  this->lowPower = 0;
-  this->rate     = 0x0A;
+  this->low_power   = 0;
+  this->rate        = 0x0A;
 }
 
-uint8_t ADXL345::BwRateBits::toByte() {
+uint8_t ADXL345::BwRateBits::to_byte() {
   uint8_t bits = 0x00;
-  bits |= this->lowPower << 4;
+  bits |= this->low_power << 4;
   bits |= this->rate;
   return bits;
 }
--- FluidNC/src/ADXL345.h
+++ FluidNC/src/ADXL345.h
@@ -16,120 +16,117 @@ typedef enum {
 
 // Register Map
 // Order is "Type, Reset Value, Description".
-#define REG_DEVID          0x00    // R,     11100101,   Device ID
-#define REG_THRESH_TAP     0x1D    // R/W,   00000000,   Tap threshold
-#define REG_OFSX           0x1E    // R/W,   00000000,   X-axis offset
-#define REG_OFSY           0x1F    // R/W,   00000000,   Y-axis offset
-#define REG_OFSZ           0x20    // R/W,   00000000,   Z-axis offset
-#define REG_DUR            0x21    // R/W,   00000000,   Tap duration
-#define REG_LATENT         0x22    // R/W,   00000000,   Tap latency 
-#define REG_WINDOW         0x23    // R/W,   00000000,   Tap window
-#define REG_THRESH_ACT     0x24    // R/W,   00000000,   Activity threshold
-#define REG_THRESH_INACT   0x25    // R/W,   00000000,   Inactivity threshold
-#define REG_TIME_INACT     0x26    // R/W,   00000000,   Inactivity time
-#define REG_ACT_INACT_CTL  0x27    // R/W,   00000000,   Axis enable control for activity and inactiv ity detection
-#define REG_THRESH_FF      0x28    // R/W,   00000000,   Free-fall threshold
-#define REG_TIME_FF        0x29    // R/W,   00000000,   Free-fall time
-#define REG_TAP_AXES       0x2A    // R/W,   00000000,   Axis control for single tap/double tap
-#define REG_ACT_TAP_STATUS 0x2B    // R,     00000000,   Source of single tap/double tap
-#define REG_BW_RATE        0x2C    // R/W,   00001010,   Data rate and power mode control
-#define REG_POWER_CTL      0x2D    // R/W,   00000000,   Power-saving features control
-#define REG_INT_ENABLE     0x2E    // R/W,   00000000,   Interrupt enable control
-#define REG_INT_MAP        0x2F    // R/W,   00000000,   Interrupt mapping control
-#define REG_INT_SOUCE      0x30    // R,     00000010,   Source of interrupts
-#define REG_DATA_FORMAT    0x31    // R/W,   00000000,   Data format control
-#define REG_DATAX0         0x32    // R,     00000000,   X-Axis Data 0
-#define REG_DATAX1         0x33    // R,     00000000,   X-Axis Data 1
-#define REG_DATAY0         0x34    // R,     00000000,   Y-Axis Data 0
-#define REG_DATAY1         0x35    // R,     00000000,   Y-Axis Data 1
-#define REG_DATAZ0         0x36    // R,     00000000,   Z-Axis Data 0
-#define REG_DATAZ1         0x37    // R,     00000000,   Z-Axis Data 1
-#define REG_FIFO_CTL       0x38    // R/W,   00000000,   FIFO control
-#define REG_FIFO_STATUS    0x39    // R,     00000000,   FIFO status
-
-// I2C Address
-#define ADXL345_STD     0x1D    // Standard address if SDO/ALT ADDRESS is HIGH.
-#define ADXL345_ALT     0x53    // Alternate address if SDO/ALT ADDRESS is LOW.
+#define ADXL345_REG_DEVID               0x00    // R,     11100101,   Device ID
+#define ADXL345_REG_THRESH_TAP          0x1D    // R/W,   00000000,   Tap threshold
+#define ADXL345_REG_OFSX                0x1E    // R/W,   00000000,   X-axis offset
+#define ADXL345_REG_OFSY                0x1F    // R/W,   00000000,   Y-axis offset
+#define ADXL345_REG_OFSZ                0x20    // R/W,   00000000,   Z-axis offset
+#define ADXL345_REG_DUR                 0x21    // R/W,   00000000,   Tap duration
+#define ADXL345_REG_LATENT              0x22    // R/W,   00000000,   Tap latency 
+#define ADXL345_REG_WINDOW              0x23    // R/W,   00000000,   Tap window
+#define ADXL345_REG_THRESH_ACT          0x24    // R/W,   00000000,   Activity threshold
+#define ADXL345_REG_THRESH_INACT        0x25    // R/W,   00000000,   Inactivity threshold
+#define ADXL345_ADXL345_REG_TIME_INACT  0x26    // R/W,   00000000,   Inactivity time
+#define ADXL345_REG_ACT_INACT_CTL       0x27    // R/W,   00000000,   Axis enable control for activity and inactiv ity detection
+#define ADXL345_REG_THRESH_FF           0x28    // R/W,   00000000,   Free-fall threshold
+#define ADXL345_REG_TIME_FF             0x29    // R/W,   00000000,   Free-fall time
+#define ADXL345_REG_TAP_AXES            0x2A    // R/W,   00000000,   Axis control for single tap/double tap
+#define ADXL345_REG_ACT_TAP_STATUS      0x2B    // R,     00000000,   Source of single tap/double tap
+#define ADXL345_REG_BW_RATE             0x2C    // R/W,   00001010,   Data rate and power mode control
+#define ADXL345_REG_POWER_CTL           0x2D    // R/W,   00000000,   Power-saving features control
+#define ADXL345_REG_INT_ENABLE          0x2E    // R/W,   00000000,   Interrupt enable control
+#define ADXL345_REG_INT_MAP             0x2F    // R/W,   00000000,   Interrupt mapping control
+#define ADXL345_REG_INT_SOUCE           0x30    // R,     00000010,   Source of interrupts
+#define ADXL345_REG_DATA_FORMAT         0x31    // R/W,   00000000,   Data format control
+#define ADXL345_REG_DATAX0              0x32    // R,     00000000,   X-Axis Data 0
+#define ADXL345_REG_DATAX1              0x33    // R,     00000000,   X-Axis Data 1
+#define ADXL345_REG_DATAY0              0x34    // R,     00000000,   Y-Axis Data 0
+#define ADXL345_REG_DATAY1              0x35    // R,     00000000,   Y-Axis Data 1
+#define ADXL345_REG_DATAZ0              0x36    // R,     00000000,   Z-Axis Data 0
+#define ADXL345_REG_DATAZ1              0x37    // R,     00000000,   Z-Axis Data 1
+#define ADXL345_REG_FIFO_CTL            0x38    // R/W,   00000000,   FIFO control
+#define ADXL345_REG_FIFO_STATUS         0x39    // R,     00000000,   FIFO status
 
 // Data Rate
-#define ADXL345_RATE_3200HZ   0x0F    // 3200 Hz
-#define ADXL345_RATE_1600HZ   0x0E    // 1600 Hz
-#define ADXL345_RATE_800HZ    0x0D    // 800 Hz
-#define ADXL345_RATE_400HZ    0x0C    // 400 Hz
-#define ADXL345_RATE_200HZ    0x0B    // 200 Hz
-#define ADXL345_RATE_100HZ    0x0A    // 100 Hz
-#define ADXL345_RATE_50HZ     0x09    // 50 Hz
-#define ADXL345_RATE_25HZ     0x08    // 25 Hz
-#define ADXL345_RATE_12_5HZ   0x07    // 12.5 Hz
-#define ADXL345_RATE_6_25HZ   0x06    // 6.25 Hz
-#define ADXL345_RATE_3_13HZ   0x05    // 3.13 Hz
-#define ADXL345_RATE_1_56HZ   0x04    // 1.56 Hz
-#define ADXL345_RATE_0_78HZ   0x03    // 0.78 Hz
-#define ADXL345_RATE_0_39HZ   0x02    // 0.39 Hz
-#define ADXL345_RATE_0_20HZ   0x01    // 0.20 Hz
-#define ADXL345_RATE_0_10HZ   0x00    // 0.10 Hz
+#define ADXL345_RATE_3200HZ             0x0F    // 3200 Hz
+#define ADXL345_RATE_1600HZ             0x0E    // 1600 Hz
+#define ADXL345_RATE_800HZ              0x0D    // 800 Hz
+#define ADXL345_RATE_400HZ              0x0C    // 400 Hz
+#define ADXL345_RATE_200HZ              0x0B    // 200 Hz
+#define ADXL345_RATE_100HZ              0x0A    // 100 Hz
+#define ADXL345_RATE_50HZ               0x09    // 50 Hz
+#define ADXL345_RATE_25HZ               0x08    // 25 Hz
+#define ADXL345_RATE_12_5HZ             0x07    // 12.5 Hz
+#define ADXL345_RATE_6_25HZ             0x06    // 6.25 Hz
+#define ADXL345_RATE_3_13HZ             0x05    // 3.13 Hz
+#define ADXL345_RATE_1_56HZ             0x04    // 1.56 Hz
+#define ADXL345_RATE_0_78HZ             0x03    // 0.78 Hz
+#define ADXL345_RATE_0_39HZ             0x02    // 0.39 Hz
+#define ADXL345_RATE_0_20HZ             0x01    // 0.20 Hz
+#define ADXL345_RATE_0_10HZ             0x00    // 0.10 Hz
 
 // Range
-#define ADXL345_RANGE_2G      0x00    // +-2 g
-#define ADXL345_RANGE_4G      0x01    // +-4 g
-#define ADXL345_RANGE_8G      0x02    // +-8 g
-#define ADXL345_RANGE_16G     0x03    // +-16 g
+#define ADXL345_RANGE_2G                0x00    // +-2 g
+#define ADXL345_RANGE_4G                0x01    // +-4 g
+#define ADXL345_RANGE_8G                0x02    // +-8 g
+#define ADXL345_RANGE_16G               0x03    // +-16 g
 
-#define ADXL345_GRAVITY_STD   (9.80665F) // Earth's gravity in m/s^2
+// Conversions
+#define ADXL345_GRAVITY_STD             (9.80665F) // Earth's gravity in m/s^2
 
 class ADXL345 {
   private:
     class PowerCtlBits {
       public:
         uint8_t link;       // D5
-        uint8_t autoSleep;  // D4
+        uint8_t auto_sleep; // D4
         uint8_t measure;    // D3
         uint8_t sleep;      // D2
         uint8_t wakeup;     // D1 - D0
 
         PowerCtlBits();
 
-        uint8_t toByte();
+        uint8_t to_byte();
     };
 
     class DataFormatBits {
       public:
-        uint8_t selfTest;   // D7
+        uint8_t self_test;  // D7
         uint8_t spi;        // D6
-        uint8_t intInvert;  // D5
-        uint8_t fullRes;    // D3
+        uint8_t int_invert; // D5
+        uint8_t full_res;   // D3
         uint8_t justify;    // D2
         uint8_t range;      // D1 - D0
 
         DataFormatBits();
 
-        uint8_t toByte();
+        uint8_t to_byte();
     };
 
     class BwRateBits {
       public:
-        uint8_t lowPower;   // D4
+        uint8_t low_power;  // D4
         uint8_t rate;       // D3 - D0
 
         BwRateBits();
 
-        uint8_t toByte();
+        uint8_t to_byte();
     };
 
-    static const float kRatio2g;
-    static const float kRatio4g;
-    static const float kRatio8g;
-    static const float kRatio16g;
+    static constexpr float k_ratio_2g = (float) (2 * 2) / 1024.0f;
+    static constexpr float k_ratio_4g = (float) (4 * 2) / 1024.0f;
+    static constexpr float k_ratio_8g = (float) (8 * 2) / 1024.0f;
+    static constexpr float k_ratio_16g = (float) (16 * 2) / 1024.0f;
 
     uint8_t _address;
     I2CBus* _i2c;
     int16_t _xyz[3];
-    PowerCtlBits _powerCtlBits;
-    DataFormatBits _dataFormatBits;
-    BwRateBits _bwRateBits;
+    PowerCtlBits _power_ctl_bits;
+    DataFormatBits _data_format_bits;
+    BwRateBits _bw_rate_bits;
 
-    float convertToG(int16_t rawValue);
-    float convertToMetersPerSec2(int16_t rawValue);
+    float convertToG(int16_t raw_value);
+    float convertToMetersPerSec2(int16_t raw_value);
 
     bool write(uint8_t value);
     bool write(uint8_t *values, size_t size);
